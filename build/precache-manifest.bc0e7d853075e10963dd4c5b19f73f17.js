f880967bb5950701f543cbcc5ba8838d43347fcf
Prasetyo Dimas <prasetyo.dimas@pricebook.co.id>
2018-11-13 11:01:29 +0700

    update all directories subfollders

---
+++ build/precache-manifest.bc0e7d853075e10963dd4c5b19f73f17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb1953bc2731ce6da447",
+    "url": "/static/css/main.d15f1f67.chunk.css"
+  },
+  {
+    "revision": "cb1953bc2731ce6da447",
+    "url": "/static/js/main.cb1953bc.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "76e79145ec2999935741db93c9f0e03a",
+    "url": "/index.html"
+  }
+];
