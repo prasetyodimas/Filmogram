9866e0eef4f82281d18b5ef141a8f881b150d96b
Dimas Prasetyo <prasetyo.dimas@pricebook.co.id>
2019-06-13 08:56:57 +0700

    UNSUPORTED: fix structure for react dom element

--- src/pages/gallery/GalleryMovies.jsx
+++ src/pages/gallery/GalleryMovies.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 //component
 import BannerTops from '../../components/BannerAdstop';
 import Headers from '../../components/Headers';
@@ -11,67 +12,31 @@ class GalleryMovies extends Component {
 	render() {
 		return (
 			<div className="wrapper">
-        <BannerTops/>
-        <Headers/>
-        <Search/>
-    	  <section className="container">
-          <div className="col-sm-12">
-            <h2 className="page-heading">Gallery</h2>
-            <div className="row">
-            <div className="gallery-wrapper">
-              <div className="col-sm-4 col-md-3">
-                <div className="gallery-item">
-                  <a href='http://placehold.it/2150x1200' className="gallery-item__image gallery-item--photo">
-                    <img alt='' src="http://placehold.it/524x524"/>
-                  </a>
-                  <a href='http://placehold.it/2150x1200' className="gallery-item__descript gallery-item--photo-link">
-                    <span className="gallery-item__icon"><i className="fa fa-camera"></i></span>
-                    <p className="gallery-item__name">Marble Arch Odeon</p>
-                  </a>
-                </div>
-              </div>
-
-              <div className="col-sm-4 col-md-3">
-                <div className="gallery-item">
-                  <a href='http://placehold.it/2150x1200' className="gallery-item__image gallery-item--photo">
-                    <img alt='' src="http://placehold.it/524x524"/>
-                  </a>
-                  <a href='http://placehold.it/2150x1200' className="gallery-item__descript gallery-item--photo-link">
-                    <span className="gallery-item__icon"><i className="fa fa-camera"></i></span>
-                    <p className="gallery-item__name">Camden Town Odeon</p>
-                  </a>
-                </div>
-              </div>
-
-              <div className="col-sm-4 col-md-3">
-                <div className="gallery-item">
-                  <a href='http://placehold.it/2150x1200' className="gallery-item__image gallery-item--photo">
-                    <img alt='' src="http://placehold.it/524x524"/>
-                  </a>
-                  <a href='http://placehold.it/2150x1200' className="gallery-item__descript gallery-item--photo-link">
-                    <span className="gallery-item__icon"><i className="fa fa-camera"></i></span>
-                    <p className="gallery-item__name">Camden Town Odeon</p>
-                  </a>
-                </div>
-              </div>
-
-              <div className="col-sm-4 col-md-3">
-                <div className="gallery-item">
-                  <a href='https://www.youtube.com/watch?v=Y5AehBA3IsE' className="gallery-item__image gallery-item--video">
-                    <img alt='' src="http://placehold.it/524x524"/>
-                  </a>
-                  <a href='https://www.youtube.com/watch?v=Y5AehBA3IsE' className="gallery-item__descript gallery-item--video-link">
-                    <span className="gallery-item__icon"><i className="fa fa-video-camera"></i></span>
-                    <p className="gallery-item__name">Camden Town Odeon</p>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        </section>
-       	<FooterApp/>
-      </div>
+				<BannerTops/>
+				<Headers/>
+				<Search/>
+				<section className="container">
+					<div className="col-sm-12">
+						<h2 className="page-heading">Gallery</h2>
+						<div className="row">
+							<div className="gallery-wrapper">
+								<div className="col-sm-4 col-md-3">
+									<div className="gallery-item">
+										<Link to="http://placehold.it/2150x1200" className="gallery-item__image gallery-item--photo">
+											<img alt="" src="http://placehold.it/524x524"/>
+										</Link>
+										<Link to="http://placehold.it/2150x1200" className="gallery-item__descript gallery-item--photo-link">
+											<span className="gallery-item__icon"><i className="fa fa-camera"></i></span>
+											<p className="gallery-item__name">Marble Arch Odeon</p>
+										</Link>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+       			<FooterApp/>
+      		</div>
 		);
 	}
 }
